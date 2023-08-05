a8f559a76a66ef8b09b0a8f8740f9c28dcdfee47
Ramkumar R <indianairforceall@gmail.com>
2023-08-05 17:07:47 +0530

    booking count API

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // index.js
 const express = require("express");
 const mongoose = require("mongoose");
-// const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = 3000;
@@ -11,8 +10,6 @@ mongoose.connect("mongodb://localhost:27017/ywait_stg_insta_gcc", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
-// app.use(bodyParser.json());
 
 // Implement the API to fetch booking details on a particular date
 app.get("/appointments", async (req, res) => {
@@ -82,6 +79,61 @@ app.get("/appointments", async (req, res) => {
   } catch (error) {
     console.error("Error fetching bookings:", error);
     res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+
+// Implement the API to get average booking count between two dates grouped by each date
+app.get('/averageBookingCount', async (req, res) => {
+  try {
+    const { user_id, business_id, from_date, to_date } = req.query;
+    const db = mongoose.connection.db;
+
+     // validation for user & business
+     const admin = await db.collection("admins").findOne({
+      _id: new mongoose.Types.ObjectId(user_id),
+      business_id: new mongoose.Types.ObjectId(business_id),
+      is_blocked: false,
+      status: "ACTIVE"
+    });
+    if(!admin) {
+      res.status(400).json({ message: "Invalid User or Business" });
+    }
+
+
+    const fromDate = new Date(from_date);
+    fromDate.setUTCHours(0, 0, 0, 0);
+    const toDate = new Date(to_date);
+    toDate.setUTCHours(23, 59, 59, 999);
+
+    const pipeline = [
+      {
+        $match: {
+          business_id: new mongoose.Types.ObjectId(business_id),
+          dateFrom: { $gte: fromDate, $lte: toDate },
+        },
+      },
+      {
+        $group: {
+          _id: { $dateToString: { format: "%Y-%m-%d", date: "$dateFrom" } },
+          bookingCount: { $sum: 1 },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          date: "$_id",
+          bookingCount: 1,
+        },
+      },
+    ];
+
+    const averageBookingCounts = await db.collection('bookings').aggregate(pipeline).toArray();
+
+    res.json(averageBookingCounts);
+  } catch (error) {
+    console.error('Error getting average booking count:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
